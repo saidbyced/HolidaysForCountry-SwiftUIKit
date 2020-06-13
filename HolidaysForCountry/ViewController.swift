39b6eb327554d3ccdf08169111bf47f1a218d213
Chris Eadie <chris.eadie@outlook.com>
2020-06-13 13:54:47 +0100

    Initial Commit

---
+++ HolidaysForCountry/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolidaysForCountry
+//
+//  Created by Chris Eadie on 13/06/2020.
+//  Copyright © 2020 ChrisEadieDesigns. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
